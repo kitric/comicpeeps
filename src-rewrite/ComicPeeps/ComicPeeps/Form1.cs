ee62c503df7e32e2900fcedb8c6c9b81e19bf3ec
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-01-20 20:01:46 +0000

    Update Form1.cs

--- src-rewrite/ComicPeeps/ComicPeeps/Form1.cs
+++ src-rewrite/ComicPeeps/ComicPeeps/Form1.cs
@@ -13,11 +13,25 @@ namespace ComicPeeps
 {
     public partial class Form1 : Form
     {
+        UserControl CurrentScreen = new UserControl();
+
         public Form1()
         {
             InitializeComponent();
 
             pnlContent.Controls.Add(new Home() { Dock = DockStyle.Fill });
+        }
+
+        public void ShowNewPage(UserControl ToOpen)
+        {
+            ToOpen.Dock = DockStyle.Fill;
+
+            pnlContent.Controls.Remove(CurrentScreen);
+
+            ToOpen.Dock = DockStyle.Fill;
+            pnlContent.Controls.Add(ToOpen);
+
+            CurrentScreen = ToOpen;
         }
     }
 }
