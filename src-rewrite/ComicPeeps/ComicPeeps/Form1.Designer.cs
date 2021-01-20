ec98d3ac187d29ce0a0dba27530a25cdfc816f04
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-01-20 19:38:11 +0000

    Begin rewrite

---
+++ src-rewrite/ComicPeeps/ComicPeeps/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace ComicPeeps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.pnlSeparator = new System.Windows.Forms.Panel();
+            this.pbIcon = new System.Windows.Forms.PictureBox();
+            this.pnlSideBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.BackColor = System.Drawing.Color.Black;
+            this.pnlSideBar.Controls.Add(this.pbIcon);
+            this.pnlSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(200, 540);
+            this.pnlSideBar.TabIndex = 0;
+            // 
+            // pnlSeparator
+            // 
+            this.pnlSeparator.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(142)))), ((int)(((byte)(68)))));
+            this.pnlSeparator.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSeparator.Location = new System.Drawing.Point(200, 0);
+            this.pnlSeparator.Name = "pnlSeparator";
+            this.pnlSeparator.Size = new System.Drawing.Size(3, 540);
+            this.pnlSeparator.TabIndex = 1;
+            // 
+            // pbIcon
+            // 
+            this.pbIcon.Image = global::ComicPeeps.Properties.Resources.appIcon;
+            this.pbIcon.Location = new System.Drawing.Point(75, 34);
+            this.pbIcon.Name = "pbIcon";
+            this.pbIcon.Size = new System.Drawing.Size(49, 49);
+            this.pbIcon.TabIndex = 0;
+            this.pbIcon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(2)))));
+            this.ClientSize = new System.Drawing.Size(960, 540);
+            this.Controls.Add(this.pnlSeparator);
+            this.Controls.Add(this.pnlSideBar);
+            this.MinimumSize = new System.Drawing.Size(976, 579);
+            this.Name = "Form1";
+            this.Text = "ComicPeeps";
+            this.pnlSideBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Panel pnlSeparator;
+        private System.Windows.Forms.PictureBox pbIcon;
+    }
+}
+
