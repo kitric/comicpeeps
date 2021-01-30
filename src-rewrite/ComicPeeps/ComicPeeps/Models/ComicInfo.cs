78399033d1cded6aacaade8307fcdf4a6374e868
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-01-30 13:36:47 +0000

    Sarted metadata editing

--- src-rewrite/ComicPeeps/ComicPeeps/Models/ComicInfo.cs
+++ src-rewrite/ComicPeeps/ComicPeeps/Models/ComicInfo.cs
@@ -8,30 +8,41 @@ namespace ComicPeeps.Models
 	public class ComicInfo
 	{
 		[XmlElement(ElementName = "Series")]
-		public string Series { get; set; }
+		public string Series { get; set; } = "";
+
 		[XmlElement(ElementName = "Number")]
-		public string Number { get; set; }
+		public string Number { get; set; } = "";
+
 		[XmlElement(ElementName = "Web")]
-		public string Web { get; set; }
+		public string Web { get; set; } = "";
+
 		[XmlElement(ElementName = "Volume")]
-		public string Volume { get; set; }
+		public string Volume { get; set; } = "";
+
 		[XmlElement(ElementName = "Summary")]
-		public string Summary { get; set; }
+		public string Summary { get; set; } = "";
+
 		[XmlElement(ElementName = "Notes")]
-		public string Notes { get; set; }
+		public string Notes { get; set; } = "";
+
 		[XmlElement(ElementName = "Publisher")]
-		public string Publisher { get; set; }
+		public string Publisher { get; set; } = "";
+
 		[XmlElement(ElementName = "Genre")]
-		public string Genre { get; set; }
+		public string Genre { get; set; } = "";
+
 		[XmlElement(ElementName = "PageCount")]
-		public string PageCount { get; set; }
+		public string PageCount { get; set; } = "";
+
 		[XmlElement(ElementName = "LanguageISO")]
-		public string LanguageISO { get; set; }
+		public string LanguageISO { get; set; } = "";
+
 		[XmlElement(ElementName = "ScanInformation")]
-		public string ScanInformation { get; set; }
+		public string ScanInformation { get; set; } = "";
+
 		[XmlAttribute(AttributeName = "xsd", Namespace = "http://www.w3.org/2000/xmlns/")]
-		public string Xsd { get; set; }
+		public string Xsd { get; set; } = "";
 		[XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
-		public string Xsi { get; set; }
+		public string Xsi { get; set; } = "";
 	}
 }
