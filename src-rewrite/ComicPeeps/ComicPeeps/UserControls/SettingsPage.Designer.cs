2a3f4d919f95d48451b46e1e4723eccddd52cf09
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-02-02 18:43:28 +0000

    Started Settings

---
+++ src-rewrite/ComicPeeps/ComicPeeps/UserControls/SettingsPage.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace ComicPeeps.UserControls
+{
+    partial class SettingsPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SettingsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Name = "SettingsPage";
+            this.Size = new System.Drawing.Size(757, 540);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
